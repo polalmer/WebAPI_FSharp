3bc1e11fdaa987e2463c00455c6a7a0bf34055d6
Jonas Palzer <j.palzer@queo.onmicrosoft.com>
2024-07-16 09:54:38 +0200

    MathController

---
+++ Controllers/MathController.fs
@@ -0,0 +1,25 @@
+﻿namespace WebAPI_FSharp.Controllers
+
+open Microsoft.AspNetCore.Mvc
+open Microsoft.Extensions.Logging
+
+[<ApiController>]
+[<Route("[controller]")>]
+type MathController (logger : ILogger<MathController>) =
+    inherit ControllerBase()
+
+    [<HttpPost("Plus")>]
+    member _.Plus(a:float,b:float) =
+        a + b
+
+    [<HttpPost("Minus")>]
+    member _.Minus(a:float,b:float) =
+        a - b
+
+    [<HttpPost("multiplikation")>]
+    member _.Multiplikation(a:float,b:float) =
+        a * b
+
+    [<HttpPost("division")>]
+    member _.Division(a:float,b:float) =
+        a / b
